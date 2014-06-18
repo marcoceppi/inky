4343df1b4d74aa7a44362bb0038acb0856d39b2f
Marco Ceppi <marco@ceppi.net>
2014-06-18 11:18:20 -0400

    Initial commit

---
+++ InkaBinka_files/siteConfig.js
@@ -0,0 +1,7 @@
+var SiteConfig = {
+    'api': {
+        'main': 'http://live-api-js.inkabinka.com',
+        'other': 'http://live-api-ms.inkabinka.com'
+    },
+    'cdnURL': 'http://g3762a7250a2f7db2a225341952de2fe9.cdn.hpcloudsvc.com/'
+};
